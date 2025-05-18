948d619d80043fe8b2a24d742c49f46682ba935f
Guilherme Quincó <guilhermeqcorrea@gmail.com>
2025-05-18 12:14:49 -0400

    chore:  corrige depedências

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -44,7 +44,6 @@ export const Modal = ({ isOpen, setIsOpen, onClose, user = null }) => {
   };
 
   const onSubmit = (e) => {
-    e.preventDefault();
     user ? updateUser() : createUser();
   };
 
