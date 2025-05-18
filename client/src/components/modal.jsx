10e0d3540df135fd2b864b3e83644ecd479f5269
Guilherme Quincó <guilhermeqcorrea@gmail.com>
2025-05-18 15:07:02 -0400

    feat: adiciona senha do usuário

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Eye } from "lucide-react";
+import { EyeClosed } from "lucide-react";
 
 export const Modal = ({ isOpen, setIsOpen, onClose, user = null }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [status, setStatus] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     if (!isOpen) return;
@@ -21,6 +25,7 @@ export const Modal = ({ isOpen, setIsOpen, onClose, user = null }) => {
       await axios.post("http://localhost:3333/users", {
         name,
         email,
+        status,
       });
       setIsLoading(false);
       router;
@@ -57,12 +62,31 @@ export const Modal = ({ isOpen, setIsOpen, onClose, user = null }) => {
             onChange={(e) => setName(e.target.value)}
             value={name}
           />
+
           <input
             type="text"
             placeholder="E-mail"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
+
+          <div className="box-input">
+            <input
+              type={isVisible ? "text" : "password"}
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+
+            <button onClick={() => setIsVisible((prev) => !prev)} id="visible">
+              {isVisible ? (
+                <EyeClosed className="icon" color="#fff" />
+              ) : (
+                <Eye className="icon" color="#fff" />
+              )}
+            </button>
+          </div>
+
           <select onChange={(e) => setStatus(e.target.value)}>
             <option value="">Selecione uma Opção</option>
             <option value="ativo">Ativo</option>
